cb42a26ac49d68a36f85e173d89a13adfa1b8781
Lekshmi1234 <lechubalaj123@gmail.com>
2019-03-12 14:03:49 +0530

    inh

---
+++ Inheritance.java
@@ -0,0 +1,20 @@
+class A{
+    public void Car(){
+        System.out.println("Audi car");
+    }
+}
+
+class B extends A{
+    public void Bike()
+    {
+        System.out.println("Duke bike");
+
+    }
+}
+
+public class Inheritance{
+    public static void main(String[] args) {
+        B ob=new B();
+        ob.Car();
+    }
+}
