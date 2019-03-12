7611c0bdbbfd37e6af94e395c7d50903ae1ad927
Lekshmi1234 <lechubalaj123@gmail.com>
2019-03-12 11:51:35 +0530

    reverse

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Reverse{
+    public static void main(String[] args) {
+        int a,rem,rev=0;
+     Scanner myinput=new Scanner(System.in);
+     
+     System.out.println("Enter the number");
+     a=myinput.nextInt();
+     while(a>0)
+     {
+     rem=a%10;
+     rev=rev*10+rem;
+     a=a/10;
+     }
+    System.out.println(rev);
+}
+}
