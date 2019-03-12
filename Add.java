dbc605494f2dd73052bc4e255cc22b6b2efa3a93
Lekshmi1234 <lechubalaj123@gmail.com>
2019-03-12 11:14:41 +0530

    Myfirstpgm

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Add{
+    public static void main(String[] args) {
+Scanner myinput=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter 1st Number");
+        a=myinput.nextInt();
+        System.out.println("Enter 2nd Number");
+        b=myinput.nextInt();
+        c=a+b;
+        System.out.println(c); 
+
+    }
+}
